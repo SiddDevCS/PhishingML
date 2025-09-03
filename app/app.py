d04beff96cff8acb5151904fc5bed67e44bce0b3
Sidd0Hari <65342042+Siddharth11sehgal@users.noreply.github.com>
2025-09-03 21:02:35 +0200

    Fixes

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import joblib
 from extract_features import extract_features
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +23,5 @@ def index():
     return render_template("index.html", prediction=prediction, url=url)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
